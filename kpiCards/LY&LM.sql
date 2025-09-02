1ae5a3c819b3fc841ecd2fc5ae7333b59f5b7ea3
Techie <149437995+FarRza111@users.noreply.github.com>
2025-09-02 20:25:08 +0300

    Update LY&LM.sql

--- kpiCards/LY&LM.sql
+++ kpiCards/LY&LM.sql
@@ -3,6 +3,7 @@
 
 */
 
+
 Last Year Full Month = 
 
 [Order Date] >= DATEADD("month", -13, DATETRUNC("month", {MAX([Order Date])}))
@@ -16,17 +17,46 @@ AND
 [Order Date] < DATETRUNC("month", {MAX([Order Date])})
 
 
+
+  
 Profit | LY =  IF [Last Year Full Month] THEN [Profit] END
 
 Profit | LM = IF [Recent Full Month] THEN [Profit] END
 
-  
-
 Profit | % LMLY = (SUM([Profit | LM]) - SUM([Profit | LY]))/SUM([Profit | LY])  
+
+  
+---------------------------- Signs ---------------------------------------------------
 
 Profit | % LMLY | Down =  IF [Profit | % LMLY] <= 0 THEN "▼" END
 
 Profit | % LMLY | Up = IF [Profit | % LMLY] > 0 THEN "▲" END
+
+
+--------------------------- Portfolio Ratios -----------------------------------------
+  
+Profit Ratio | LM = 
+
+SUM(IF [Recent Full Month]
+THEN [Profit]
+END)
+/
+SUM(IF [Recent Full Month]
+THEN [Sales]
+END)
+
+Profit Ratio | LY = 
+
+SUM(IF [Last Year Full Month]
+THEN [Profit]
+END)
+/
+SUM(IF [Last Year Full Month]
+THEN [Sales]
+END)
+
+
+Profit Ratio | % LMLY = ([Profit Ratio | LM] - [Profit Ratio | LY])/[Profit Ratio | LY]
 
 
 
