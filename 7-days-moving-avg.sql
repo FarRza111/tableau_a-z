19c717b157db9c2ce5b3e58d73b12c6415b0105e
Techie <149437995+FarRza111@users.noreply.github.com>
2025-09-03 14:34:38 +0300

    Update 7-days-moving-avg.sql

--- 7-days-moving-avg.sql
+++ 7-days-moving-avg.sql
@@ -1,4 +1,6 @@
-wa = 
+-- Moving Avg for 7 Days ----------
+
+_7d moving avg = 
 
 VAR period = 7
 VAR the_first_date =
@@ -12,3 +14,21 @@ VAR sum_in_period =
     )
 RETURN
     IF ( last_date - the_first_date >= period - 1, sum_in_period / period )
+
+
+
+-- Moving Sum for 7 days ---------
+
+_7d moving sum = 
+VAR period = 7
+VAR the_first_date =
+    CALCULATE ( FIRSTDATE ( MyTable[Date] ), ALLSELECTED ( MyTable[Date] ) )
+VAR last_date =
+    LASTDATE ( MyTable[Date] )
+VAR sum_in_period =
+    CALCULATE (
+        SUM ( MyTable[Value] ),
+        DATESINPERIOD ( MyTable[Date], last_date, - period, DAY )
+    )
+RETURN
+    IF ( last_date - the_first_date >= period - 1, sum_in_period )
